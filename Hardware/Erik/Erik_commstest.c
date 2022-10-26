2c7c4db93bcd90649d7f0652109a1b9ca06cf863
Erik914 <55508711+Erik914@users.noreply.github.com>
2022-10-26 03:05:32 -0500

    Update Erik_commstest.c
    Added string splitting and dumping to double.

--- Hardware/Erik/Erik_commstest.c
+++ Hardware/Erik/Erik_commstest.c
@@ -1,5 +1,6 @@
 //usr/bin/cc "$0" "comms.c" && exec ./a.out "$@"
 #include "comms.h"
+#include <string.h>
 
 #define BUFFSIZE 512
 
@@ -10,14 +11,43 @@ int main(void) {
 	int count = 0;
 	char buf[BUFFSIZE] = {0};
 	char outbuf[BUFFSIZE] = {0};
-	while(count <= 100) {
+	double heading;
+	double posx;
+	double posy;
+	while(1) {
 		n = readInPipe(pip, buf, BUFFSIZE);
-        if ( write(STDOUT_FILENO, buf, n) != n) {
-            return 1;
-        }
+        /*if ( write(STDOUT_FILENO, buf, n) != n) {
+            //return 1;
+        }*/
         sprintf(outbuf, "received: %d", count++);
-        writeOutPipe(pip, outbuf, BUFFSIZE);
-	}
+		writeOutPipe(pip, outbuf, BUFFSIZE);
+
+		/*char *token = strtok(buf, ",");
+		n = 0;
+		while(n++ < 5) {
+			printf("%s\n",token);
+			token = strtok(NULL, ",");
+		}*/
+
+		/*char *token = strtok(buf, ",");
+		sscanf(token, "%lf", &posx);
+		printf("%.4lf\n", posx);
+		token = strtok(NULL, ",");
+		sscanf(token, "%lf", &posy);
+		printf("%.4lf\n", posy);
+		token = strtok(NULL, ",");
+		//printf("%s\n", token);
+		sscanf(token, "%lf", &heading);
+		printf("%.4lf\n", heading);*/
+
+		sscanf(strtok(buf, ","), "%lf", &posx);
+		printf("%.4lf\n", posx);
+		sscanf(strtok(NULL, ","), "%lf", &posy);
+		printf("%.4lf\n", posy);
+		sscanf(strtok(NULL, ","), "%lf", &heading);
+		printf("%.4lf\n", heading);
+
+		}
 	closePipes(pip);
 	return 0;
 }
