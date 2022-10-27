7298222c87b53f10066234c51a74deae7713089a
Erik914 <55508711+Erik914@users.noreply.github.com>
2022-10-26 22:41:51 -0500

    Update Erik_allpozyx.py
    More integration

--- Hardware/Erik/Erik_allpozyx.py
+++ Hardware/Erik/Erik_allpozyx.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from pypozyx import *
 from pypozyx.structures.device_information import *
+from rpi_hardware_pwm import HardwarePWM
 from time import sleep
 from datetime import datetime
 read = datetime.now()
@@ -12,9 +13,8 @@ from pypozyx import (POZYX_POS_ALG_UWB_ONLY, POZYX_3D, Coordinates, POZYX_SUCCES
 class ReadyToLocalize(object):
 	"""Continuously calls the Pozyx positioning function and prints its position."""
 
-	def __init__(self, pozyx, osc_udp_client, anchors, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_3D, height=1000, remote_id=None):
+	def __init__(self, pozyx, anchors, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_3D, height=1000, remote_id=None):
 		self.pozyx = pozyx
-		self.osc_udp_client = osc_udp_client
 
 		self.anchors = anchors
 		self.algorithm = algorithm
@@ -50,8 +50,9 @@ class ReadyToLocalize(object):
 		if status == POZYX_SUCCESS:
 			return self.printPublishPosition(position)
 		else:
-			#self.printPublishErrorCode("positioning")
-			return Coordinates(None,None,None), -1, self.remote_id
+			#write = datetime.now()-read
+			return Coordinates(0,0,0), -1, self.remote_id
+			self.printPublishErrorCode("positioning")
 
 	def printPublishPosition(self, position):
 		"""Prints the Pozyx's position and possibly sends it as a OSC packet"""
@@ -59,9 +60,9 @@ class ReadyToLocalize(object):
 		if network_id is None:
 			network_id = 0
 		write = datetime.now()-read
+		return position, write, network_id
 		print("POS ID {}, x(mm): {pos.x} y(mm): {pos.y} z(mm): {pos.z} time: {time}".format(
 			"0x%0.4x" % network_id, pos=position, time=write))
-		return pos, write, network_id
 
 	def printPublishErrorCode(self, operation):
 		"""Prints the Pozyx's error and possibly sends it as a OSC packet"""
@@ -166,16 +167,15 @@ if __name__ == '__main__':
 		print("No Pozyx connected. Check your USB cable or your driver!")
 		quit()
 
-	remote_id = None
-
 	# enable to send position data through OSC
 	use_processing = False
 
 	# configure if you want to route OSC to outside your localhost. Networking knowledge is required.
 	ip = "127.0.0.1"
 	network_port = 8888
-
 	osc_udp_client = None
+	
+	remote_id = None#0x7624
 
 	# necessary data for calibration, change the IDs and coordinates yourself according to your measurement
 	anchors = [DeviceCoordinates(0x7604, 1, Coordinates(5734,11275,1370)),
@@ -193,20 +193,31 @@ if __name__ == '__main__':
 	height = 1000
 
 	pozyx = PozyxSerial(serial_port)
-	r = ReadyToLocalize(pozyx, osc_udp_client, anchors, algorithm, dimension, height, remote_id)
+	r = ReadyToLocalize(pozyx, anchors, algorithm, dimension, height, remote_id)
 	r.setup()
 	pos = Coordinates()
+	mag = Magnetic()
+	head = EulerAngles()
+
+	servopwm = HardwarePWM(pwm_channel=0, hz=480)
+	servoduty = 72
+	servopwm.start(servoduty) # servo
+	motorpwm = HardwarePWM(pwm_channel=1, hz=480)
+	motorduty = 72
+	motorpwm.start(72) # motor
 	while True:
 		try:
 			read = datetime.now()
 			pos, write, net_id = r.loop()
-			pos.x = 0
-			pos.y = 0
-			pos.z = 0
-			pozyx.getAllSensorData(raw)
-			os.write(out, bytes(f"{raw[4]},{raw[5]},{raw[6]},{raw[10]},{raw[11]},{raw[12]}\n\
-			x={pos.x},y={pos.y},z={pos.z},dt={write}, id={net_id}\n","utf-8"))
-			print(os.read(infile, 512).decode("utf-8"))
+			pozyx.getAllSensorData(raw, remote_id)
+			mag = Magnetic(raw[4],raw[5],raw[6])
+			head = EulerAngles(raw[10],raw[11],raw[12])
+			os.write(out, bytes(f"{pos.x/1000},{pos.y/1000},{head.heading},{write},{net_id}\n","utf-8"))
+			print(pos)
+			[motorduty, servoduty] = [float(x) for x in os.read(infile, 512).decode("utf-8").rstrip("\x00").split(",")]
+			servopwm.change_duty_cycle(servoduty)
+			motorpwm.change_duty_cycle(motorduty)
+			print(f"{motorduty},{servoduty}")
 		except:
 			try:
 				os.close(out)
